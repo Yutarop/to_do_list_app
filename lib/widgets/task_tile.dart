f13a5601ac9a72f447e4e3daef710705d3afe0ca
Yutaro <katoyutaro1122@icloud.com>
2025-03-14 15:33:32 +0900

    add functionality to the ckeckboxes

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list_app/constants.dart';
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({super.key});
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkboxCallback(bool? checkboxState){
+    setState(() {
+      isChecked = checkboxState ?? false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
         'Buy milk',
-        style: kToDoTextStyle,
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
-      trailing: Checkbox(
-          value: false,
-          onChanged: (value){}),
+      trailing: TaskCheckBox(checkboxSate: isChecked, toggleCheckboxState: checkboxCallback,),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkboxSate;
+  final void Function(bool?) toggleCheckboxState;
+  TaskCheckBox({required this.checkboxSate, required this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxSate,
+      onChanged: toggleCheckboxState,
     );
   }
 }
